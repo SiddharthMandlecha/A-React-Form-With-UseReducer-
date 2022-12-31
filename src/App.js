822c017a2aef5add2ca92ab0de5a06fb7a2a638c
Your Name <you@example.com>
2023-01-01 04:48:01 +0530

    Created a from using react hooks and UseReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./App.css";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  PasswordRepeat: "",
+  termsAccepted: false
+};
+
+function reducer(state, action) {
+  return { ...state, [action.input]: action.value };
+}
+
+function validateState(state) {
+  return (
+    state.password === state.PasswordRepeat &&
+    state.termsAccepted &&
+    state.password.length > 6
+  );
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log(state);
+
+  function handleClick(e) {
+    e.preventDefault();
+    alert(`Hey ${state.name} you have successfully registered!`);
+  }
+
+  function onChange(e) {
+    const { name, value, checked } = e.target;
+    const action = {
+      input: name,
+      value: name === "termsAccepted" ? checked : value
+    };
+    dispatch(action);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="RegisterFormContainer">
+        <h2 className="RegisterContainerHeadline">Register</h2>
+        <form className="RegisterForm">
+          <input
+            className="TextInput"
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={onChange}
+          />
+
+          <input
+            className="TextInput"
+            type="text"
+            name="email"
+            placeholder="Email"
+            onChange={onChange}
+          />
+
+          <input
+            className="TextInput"
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={onChange}
+          />
+
+          <input
+            className="TextInput"
+            type="password"
+            name="PasswordRepeat"
+            placeholder="Password repeat"
+            onChange={onChange}
+          />
+
+          <label className="TouchCheckboxLabel">
+            <input
+              className="TouCheckbox"
+              type="checkbox"
+              name="termsAccepted"
+              onChange={onChange}
+            />
+            Accept Terms Of Use!
+          </label>
+          <button
+            disabled={!validateState(state)}
+            onClick={handleClick}
+            className={!validateState(state) ? "Disabled" : null}
+          >
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
